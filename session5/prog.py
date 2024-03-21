1a50ce8a34a007775e0cb31b70f8c18917094276
Avni Goel <avnigoel98@gmail.com>
2024-03-22 00:01:50 +0530

    notes for networking

---
+++ session5/prog.py
@@ -0,0 +1,78 @@
+#list
+a = [5, 7, 9, 8, 5]
+
+#tuple
+b = (5, 8, 6, 9, 4)
+
+# a[2] = 3
+
+# b[1] = 6
+
+# print(a[2])
+
+# print(b[1])
+
+
+# a = [5, "apple" , False, 6.8]
+
+# a = [5, 7, 9, 8, 5]
+
+
+# print(a[0:2])
+
+
+# list methods / list functions
+
+a = [5, 7, 9, 8, 5]
+# a.sort()
+
+# a.reverse()
+
+# a.append(6) # add to the last of the string
+
+# a.insert(2, 100)
+
+# a.pop(1) # will delete at index 1
+
+# a.remove(7) # will delete the value 7 from the list
+
+# write a program to accept marks of 4 students and display them in a sorted manner
+
+
+x = int(input("enter mark 1"))
+x1 = int(input("enter mark 1"))
+x2 = int(input("enter mark 1"))
+x3 = int(input("enter mark 1"))
+
+list1 = [x,x1,x2,x3]
+
+# list1.sort()
+
+
+# length = (len(list1))
+
+# maximum_element = list1[length - 1]
+
+maximum_element = max(list1)
+print(maximum_element)
+
+
+
+
+
+# #list1.sort()
+# even = []
+# for i in range(4):
+#     if list1[i] % 2 == 0:
+#        even.append(list1[i])
+
+# print(even)
+    
+
+
+
+# counting elements in a list
+
+a = [5, 8, 6, 5, 6, 5, 8]
+
+
